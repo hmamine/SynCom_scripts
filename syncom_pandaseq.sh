08b42c858b7287b29130a7a72bba6853d5c63ca7
mahassani <hassani@mpipz.mpg.de>
2017-08-17 17:24:31 +0200

    Update syncom_pandaseq.sh

--- syncom_pandaseq.sh
+++ syncom_pandaseq.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 
-#sampleIDlist => Need to be defined and it corresponds to the names of the samples 
-#otuIDlist => Need to be defined and it corresponds to the names of isolates from the 16s fasta file
-#16s_trim_v5v6_KG_DepExp_Derep.fa => need to be defined and it corresponds to the 16s trimmed reference sequences
+#sampleIDlist => Need to be defined and must correspond to the names of the samples 
+#otuIDlist => Need to be defined and must correspond to the names of isolates 
+#16s_trim_v5v6_KG_DepExp_Derep.fa => is a fasta file, needs to be defined and must be trimmed
 
 #Uncompress MiSeq files
 gzip -d  001_forward_reads.fastq.gz
@@ -19,17 +19,14 @@ pandaseq -N -o 80 -q 30 -F -d rbfkms -l 344 -L 389 -f 001_forward_reads_tagged.f
 #Name assembled read by IdSample
 $path_to_script/NameBySample.py assembled.fastq 002_mapping.txt  fasta -
 
-#Get IdSamples DO NOT forget to delete header
 #cat  ../Rawseq/001_mapping.txt | sed -e 's/\t.*//g' > idsamples.txt
 #cat  list | sed -e 's/.fa//g' > idsamples
-
-#Get otuUds
 #grep ">" 16s_exp.fa | sed -e 's/ .*//g;s/>//g;'> outIDs
 
-#Split the seqs fasta file by Idsample names
+#Split the seqs fasta file by Idsample names for mapping
 bsub -q multicore40 -R "rusage[mem=20000]" $path_to_scripts/sortByHeaderStr.py ../seqs_pandaseq.fasta ../idsamples.txt
 
-#Aligment of reads to RefSeq
+#Aligment of reads to Reference 16S
 java -Xmx40g -jar /home/hassani/RDPTools/AlignmentTools.jar pairwise-knn -k 1 -m global -t 3 -p 0 -o outputalig/$output \
 $QUERY $path/16s_trim_v5v6_KG_DepExp_Derep.fa
 
